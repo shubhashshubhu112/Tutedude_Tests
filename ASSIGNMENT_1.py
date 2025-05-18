6cc2b2e26900f3eb766b41eb5acd1e76f10eda97
shubhashshubhu112 <shubhashnaththebull@gmail.com>
2025-05-18 19:43:31 +0530

    Add files via upload

---
+++ ASSIGNMENT_1.py
@@ -0,0 +1,12 @@
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+
+add=num1+num2
+Subtraction=num1-num2
+Multiplication=num1*num2
+Division=num1/num2
+
+print("Addition:",add)
+print("Subtraction:",Subtraction)
+print("Multiplication:",Multiplication)
+print("Division:",Division)
---
+++ Assignment_2.py
@@ -0,0 +1,6 @@
+first=str(input("Enter your first name :"))
+last=str(input("Enter your last name :"))
+
+full_name= first + " " + last
+
+print("Hello,",full_name+ "!","Welcome to the python program.")
